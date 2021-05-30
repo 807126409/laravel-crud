9c75e8ace596e3f0cbc0c9f98ddd226cd376ee69
syedarsalan9 <syedarslanahmed99@gmail.com>
2021-05-31 02:16:38 +0500

    resolving issue

--- resources/views/auth/authentication.blade.php
+++ resources/views/auth/authentication.blade.php
@@ -17,25 +17,13 @@
         @csrf      
         <div class="login-signup-content">
             <div class="input-name">
-                <h2>Email</h2>
-
+                <h2>Username</h2>
             </div>
             <input type="email" name="email" value="" class="field-input" />
-            @if ($errors->has('email'))
-                <span class="help-block">
-                    {{ $errors->first('email') }}
-                </span>
-            @endif
             <div class="input-name input-margin">
                 <h2>Password</h2>
-
             </div>
             <input type="password" name="password" value="" class="field-input" />
-            @if ($errors->has('password'))
-                <span class="help-block">
-                    {{ $errors->first('password') }}
-                </span>
-            @endif
             <div class="input-r">
                 <div class="check-input">
                     <input type="checkbox" id="remember-me-2" name="rememberme" value="" class="checkme">
@@ -63,38 +51,18 @@
                 <h2>Username</h2>
             </div>
             <input type="text" name="name" value="" class="field-input" />
-            @if ($errors->has('name'))
-                <span class="help-block">
-                    {{ $errors->first('name') }}
-                </span>
-            @endif
             <div class="input-name input-margin">
                 <h2>E-Mail</h2>
             </div>
             <input type="email" name="email" value="" class="field-input" />
-            @if ($errors->has('email'))
-                <span class="help-block">
-                    {{ $errors->first('email') }}
-                </span>
-            @endif
             <div class="input-name input-margin">
                 <h2>Password</h2>
             </div>
             <input type="password" name="password" value="" class="field-input" />
-            @if ($errors->has('password'))
-                <span class="help-block">
-                    {{ $errors->first('password') }}
-                </span>
-            @endif
             <div class="input-name input-margin">
                 <h2>Confirm Password</h2>
             </div>
             <input type="password" name="password_confirmation" value="" class="field-input" />
-            @if ($errors->has('password_confirmation'))
-                <span class="help-block">
-                    {{ $errors->first('password_confirmation') }}
-                </span>
-            @endif
             <button type="submit" class="submit-btn">
                 Register
             </button>
